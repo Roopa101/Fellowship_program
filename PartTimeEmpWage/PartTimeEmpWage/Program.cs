e204d190c30e6ed261d02f8a16ec07bb1e54c772
Roopa Jamadar <roopajamadar14@gmail.com>
2021-10-29 19:15:03 +0530

    UC3 committed

---
+++ PartTimeEmpWage/PartTimeEmpWage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PartTimeEmpWage
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int IsPartTime = 1;
+            int IsFullTime = 2;
+            int EmpRatePrHr = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IsPartTime)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IsFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EmpRatePrHr;
+            Console.WriteLine("Emp Wage= " + empWage);
+        }
+    }
+}
