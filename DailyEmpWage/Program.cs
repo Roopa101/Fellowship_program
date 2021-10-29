ac8d22b39bc06e58f77abd17c8d372e39f75b70e
Roopa Jamadar <roopajamadar14@gmail.com>
2021-10-29 18:30:10 +0530

    UC2 committed

---
+++ DailyEmpWage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DailyEmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IsFullTime = 1;
+            int EmpRatePrHr = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EmpRatePrHr;
+            Console.WriteLine("Emp Wage ="+ empWage);
+        }
+    }
+}
