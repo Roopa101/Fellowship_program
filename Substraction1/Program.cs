22416f99f4421577f3cc91b7176abea10115bcb9
Roopa Jamadar <roopajamadar14@gmail.com>
2021-10-29 15:15:57 +0530

    sub committed

---
+++ Substraction1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class cal
+    {
+        int totalvalue;
+        public int isSub(int num1, int num2)
+        {
+            return totalvalue = num1 - num2;
+        }
+        static void Main(string[] args)
+        {
+            cal objcal = new cal();
+            Console.WriteLine("total values is:" + objcal.isSub(80, 38));
+            Console.ReadKey();
+        }
+    }
+}
