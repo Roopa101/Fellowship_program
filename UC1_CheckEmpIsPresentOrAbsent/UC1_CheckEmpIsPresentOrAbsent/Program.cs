83a44d30f4665fb6f0f3e868d82e1b143f6719a5
Roopa Jamadar <roopajamadar14@gmail.com>
2021-10-29 18:08:37 +0530

    UC1_Present_Absent

---
+++ UC1_CheckEmpIsPresentOrAbsent/UC1_CheckEmpIsPresentOrAbsent/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC1_CheckEmpIsPresentOrAbsent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IsFullTime = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+
+                Console.WriteLine("Emp is Present");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Emp is Absent");
+
+            }
+
+        }
+    }
+}
