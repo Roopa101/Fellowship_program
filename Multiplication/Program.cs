3871146c8dfafe48553438f7c9d95c768af4a7f3
Roopa Jamadar <roopajamadar14@gmail.com>
2021-10-29 15:24:41 +0530

    mult committed

---
+++ Multiplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class cal
+    {
+        int totalvalue;
+        public int isMul(int num1, int num2)
+        {
+            return totalvalue = num1 * num2;
+        }
+        static void Main(string[] args)
+        {
+            cal objcal = new cal();
+            Console.WriteLine("total values is:" + objcal.isMul(80, 38));
+            Console.ReadKey();
+        }
+    }
+}
